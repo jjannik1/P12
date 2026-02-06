f2168f14d2a280410562343e831d06120f8b0d72
jjannik1 <jjen527@g.educaand.es>
2026-02-06 13:00:31 +0100

    Arreglos de la aplicacion

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,5 @@ FROM python:3.11-slim
 WORKDIR /app
 COPY --from=build /app /app
 EXPOSE 8000
+RUN pip install --no-cache-dir -r requirements.txt
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "wsgi:app"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 import logging
+import os
 from app.api.endpoints import api_bp
 from app.routes import main_bp
+
+# Crear carpeta logs si no existe
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 # Configurar logging
 logging.basicConfig(filename='logs/app.log', level=logging.INFO,
@@ -11,12 +16,12 @@ def create_app():
     app = Flask(__name__)
     
     # Registrar Blueprints
-    app.register_blueprint(main_bp)      # Rutas web
+    app.register_blueprint(main_bp)                  # Rutas web
     app.register_blueprint(api_bp, url_prefix='/api')  # Rutas API
     
     # Logging de todas las peticiones
     @app.before_request
     def log_request():
-        logging.info(f"{Flask.request.method} {Flask.request.path}")
+        logging.info(f"{request.method} {request.path}")
     
     return app
